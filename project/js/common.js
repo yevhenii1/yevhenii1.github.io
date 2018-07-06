5dcc13aad29531fbf34d83442621d4fa2f5965ac
yevhenii1 <yevheniikyvenko@gmail.com>
2018-07-06 13:03:40 +0300

    add comon.js

--- project/js/common.js
+++ project/js/common.js
@@ -63,16 +63,13 @@ function showSlides(n) {
 
  $(document).ready(function(){
  	$(".main-nav__lists").on("click","a", function (event) {
- 		//отменяем стандартную обработку нажатия по ссылке
+
  		event.preventDefault();
 
- 		//забираем идентификатор бока с атрибута href
  		var id  = $(this).attr('href'),
 
- 		//узнаем высоту от начала страницы до блока на который ссылается якорь
  			top = $(id).offset().top;
 
- 		//анимируем переход на расстояние - top за 1500 мс
  		$('body,html').animate({scrollTop: top}, 1500);
  	});
  });
